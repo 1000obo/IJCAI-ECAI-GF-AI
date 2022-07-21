fb2c27fb2793ce67fa318d07d39fc0f8565addbc
Inês Lobo <millobo97@gmail.com>
2022-07-21 21:21:33 +0100

    First Commit
    Game, levels and agents.
    Submission of random agent in Python.

---
+++ python/agent.py
@@ -0,0 +1,49 @@
+﻿import numpy as np
+from enum import Enum
+
+'''Character Moves'''
+class Moves(Enum):
+	NO_ACTION = 0
+	ROLL_LEFT = 1
+	ROLL_RIGHT = 2
+	JUMP = 3
+	GROW = 4
+	MOVE_LEFT = 5
+	MOVE_RIGHT = 6
+	MORPH_UP = 7
+	MORPH_DOWN = 8
+
+'''Agent Class'''
+class Agent:
+	def __init__(self, name, env):
+		self.name = name
+		self.env = env
+		self.currentAction = 0
+	
+	#TODO: messages between agents? 
+
+'''Circle Agent Class'''
+class CircleAgent(Agent):
+	def __init__(self, name, env):
+		super().__init__(name, env)
+		self.possibleMoves = [Moves.ROLL_LEFT.value, Moves.ROLL_RIGHT.value, Moves.JUMP.value]
+		self.lenMoves = len(self.possibleMoves)
+	
+	def randomAction(self):
+		self.currentAction = np.random.randint(0, self.lenMoves)
+		
+	def executeAction(self):
+		self.randomAction()
+
+'''Rectangle Agent Class'''
+class RectangleAgent(Agent):
+	def __init__(self, name, env):
+		super().__init__(name, env)
+		self.possibleMoves = [Moves.MOVE_LEFT.value, Moves.MOVE_RIGHT.value, Moves.MORPH_UP.value, Moves.MORPH_DOWN.value]
+		self.lenMoves = len(self.possibleMoves)
+	
+	def randomAction(self):
+		self.currentAction = np.random.randint(0, self.lenMoves)
+
+	def executeAction(self):
+		self.randomAction()
---
+++ python/main.py
@@ -0,0 +1,39 @@
+﻿import jsonManager
+import agent
+import pickle
+import sys
+
+def main():
+	if (sys.argv[1] == "initRectangleAgent"):
+		jsonMng = jsonManager.JsonManager()
+		env = jsonMng.initEnvironment(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8])
+		a = agent.RectangleAgent("Rectangle", env)
+		fileRect = open("../../python/rectangle.pickle", "wb")
+		pickle.dump(a, fileRect)
+		fileRect.close()
+	elif (sys.argv[1] == "initCircleAgent"):
+		jsonMng = jsonManager.JsonManager()
+		env = jsonMng.initEnvironment(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8])
+		a = agent.CircleAgent("Circle", env)
+		fileCircle = open("../../python/circle.pickle", "wb")
+		pickle.dump(a, fileCircle)
+		fileCircle.close()
+	elif (sys.argv[1] == "runRectangleAgent"):
+		jsonMng = jsonManager.JsonManager()
+		fileRect = open("../../python/rectangle.pickle", "rb")
+		a = pickle.load(fileRect)
+		fileRect.close()
+		jsonMng.updateEnvironment(a, sys.argv[2], sys.argv[3], sys.argv[4]) #update environment
+		a.executeAction() #execute action
+		print(a.currentAction) #do not remove this print
+	elif (sys.argv[1] == "runCircleAgent"):
+		jsonMng = jsonManager.JsonManager()
+		fileCircle = open("../../python/circle.pickle", "rb")
+		a = pickle.load(fileCircle)
+		fileCircle.close()
+		jsonMng.updateEnvironment(a, sys.argv[2], sys.argv[3], sys.argv[4]) #update environment
+		a.executeAction() #execute action
+		print(a.currentAction) #do not remove this print
+	
+if __name__ == "__main__":
+	main()
